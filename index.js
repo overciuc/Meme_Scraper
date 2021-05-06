bb72685c35ce6787e5b68e788d3f94a9a0695637
Olga Verciuc <o.verciuc@gmail.com>
2021-05-06 16:30:57 +0200

    Add comments to index.js

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const fs = require('fs');
 
+// fetch the html from url
 const site = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 axios.get(site).then((response) => {
     const html = response.data;
     const dom = cheerio.load(html);
+
+    // store the image data into an array
     const imageArray = [];
     dom('img').each((i, img) => {
+        // loop through the first 10 images and store them into the array
         if (i <= 9) {
             imageArray.push(img.attribs.src);
         }
     });
-
+    // loop through the images in the array and store them into Memes folder with .jpg extension
     for (let i = 0; i < imageArray.length; i++) {
         async function downloadImages() {
             const getImageResponse = await fetch(imageArray[i]);
             const myBlob = await getImageResponse.buffer();
             fs.writeFile(`./Memes/${i}.jpg`, myBlob, () =>
+                // display a progress message in the console
                 console.log(`Image ${i} download and saving complete`),
             );
         }
