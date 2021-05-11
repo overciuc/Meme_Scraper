aa50607b729b9aa44773c72bd8da169e6482eb7a
Olga Verciuc <o.verciuc@gmail.com>
2021-05-11 08:02:35 +0200

    Update index.js and custom_meme_scraper.js

--- custom_meme_scraper.js
+++ custom_meme_scraper.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable unicorn/prefer-node-protocol */
 const fetch = require('node-fetch');
 const fs = require('fs');
 const readline = require('readline');
@@ -9,19 +7,19 @@ const userInput2 = process.argv[3];
 const userInput3 = process.argv[4];
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const benderSite = `https://api.memegen.link/images/${userInput3}/${userInput1}/${userInput2}.png?height=350&width=600`;
 
 async function downloadImage() {
-    const getImageResponse = await fetch(benderSite);
-    const myBlob = await getImageResponse.buffer();
-    fs.writeFile('./Memes/custom.png', myBlob, () => {
-        // display a progress message in the console
-        console.log(`Image download and saving complete`);
-        process.exit(0);
-    });
+  const getImageResponse = await fetch(benderSite);
+  const myBlob = await getImageResponse.buffer();
+  fs.writeFile('./Memes/custom.png', myBlob, () => {
+    // display a progress message in the console
+    console.log(`Image download and saving complete`);
+    process.exit(0);
+  });
 }
 downloadImage();
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable unicorn/prefer-node-protocol */
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
@@ -8,28 +7,28 @@ const fs = require('fs');
 const site = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 axios.get(site).then((response) => {
-    const html = response.data;
-    const dom = cheerio.load(html);
+  const html = response.data;
+  const dom = cheerio.load(html);
 
-    // store the image data into an array
-    const imageArray = [];
-    dom('img').each((i, img) => {
-        // loop through the first 10 images and store them into the array
-        if (i <= 9) {
-            imageArray.push(img.attribs.src);
-        }
-    });
-    // loop through the images in the array and store them into Memes folder with .jpg extension
-    for (let i = 0; i < imageArray.length; i++) {
-        async function downloadImages() {
-            const getImageResponse = await fetch(imageArray[i]);
-            const myBlob = await getImageResponse.buffer();
-            fs.writeFile(`./Memes/${i}.jpg`, myBlob, () =>
-                // display a progress message in the console
-                console.log(`Image ${i} download and saving complete`),
-            );
-        }
-        downloadImages();
+  // store the image data into an array
+  const imageArray = [];
+  dom('img').each((i, img) => {
+    // loop through the first 10 images and store them into the array
+    if (i <= 9) {
+      imageArray.push(img.attribs.src);
     }
-    // console.log(imageArray);
+  });
+  // loop through the images in the array and store them into Memes folder with .jpg extension
+  for (let i = 0; i < imageArray.length; i++) {
+    async function downloadImages() {
+      const getImageResponse = await fetch(imageArray[i]);
+      const myBlob = await getImageResponse.buffer();
+      fs.writeFile(`./Memes/${i}.jpg`, myBlob, () =>
+        // display a progress message in the console
+        console.log(`Image ${i} download and saving complete`),
+      );
+    }
+    downloadImages();
+  }
+  // console.log(imageArray);
 });
